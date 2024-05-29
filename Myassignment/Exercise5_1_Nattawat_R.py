b2880e161a29bbbf16e8448c74fb598a58648230
Tai Dev_Tech <149864414+Nattawat1409@users.noreply.github.com>
2024-05-29 15:06:37 +0700

    to calculate the formula by using python
    these python program we concern about the 2 variable to perform by the less changing  for practicing and improve python programing skills.

---
+++ Myassignment/Exercise5_1_Nattawat_R.py
@@ -0,0 +1,11 @@
+num1 = int(input("select number1 : "))
+num2 = int(input("select number2 : "))
+adding = num1+num2
+subtract = num1 - num2
+mutiply = num1 * num2
+divide = num1 / num2
+
+print(num1, "+" ,num2, "=",adding)
+print(num1, "-" ,num2, "=",subtract)
+print(num1, "*" ,num2, "=" ,mutiply)
+print(num1, "/" ,num2, "=" , divide)
---
+++ Myassignment/Exercise5_2_Nattawat_R.py
@@ -0,0 +1,4 @@
+distance = int(input("input velocity: "))
+time = int(input("input time: "))
+velocity = distance / time
+print(velocity,"km/h")
