4ecbbb74c2aaa593ec5572c2e140ac7655b9c162
Tai Dev_Tech <149864414+Nattawat1409@users.noreply.github.com>
2024-06-12 17:07:01 +0700

    call diverse function

---
+++ Lecture/Lecture50.py
@@ -0,0 +1,20 @@
+def addnumber(x , y):
+    print("adding number :",x+y)
+
+def subtractnumber(x,y):
+    print("subtract number :",x-y)
+
+def multiplication(x,y):
+    print("mutiply number :",x*y)
+
+def divide(x,y):
+    print("divide number :",x/y)
+
+num1 = int(input("number 1 :"))
+num2 = int(input("number 2 :"))
+
+
+addnumber(num1,num2)
+subtractnumber(num1,num2)
+multiplication(num1,num2)
+divide(num1,num2)
